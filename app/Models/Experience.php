46b25615220ea894f64c78e8813942e9daa2f2ef
Andres F Mosquera <moscoquera@gmail.com>
2017-11-02 19:41:38 -0500

    fix a app\Models\Experience

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -10,7 +10,8 @@ class Experience extends Model
     protected $fillable=['curriculum_id','currently','company','boss','phone','start_date','end_date','retirement','functions_in_charge','company_id','sector_id'];
 
     protected $casts=[
-        'currently'=>'boolean'
+        'currently'=>'boolean',
+        'company_id'=>'integer'
     ];
 
 }
